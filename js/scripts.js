e1f4d2d06f92972354a043222f57c6f2cd8fca11
mituwebdvp <mehjabinmitu124@gmail.comm>
2024-11-08 20:19:39 +0600

    portfolio update

--- js/scripts.js
+++ js/scripts.js
@@ -30,32 +30,3 @@ document.getElementById("Latest").onclick = function(e) {
     document.getElementById("selleritems").style.display = "none";
 }
 
-
-
-
-// document.getElementById("furniture_section").onclick = function() {
-//     document.getElementById("selleritems").style.display = "block";
-//     document.getElementById("latestitems").style.display = "block";
-// }
-
-
-
-
-// document.getElementById("featureditems").onclick = function() {
-//     document.getElementById("selleritems").style.display = "none";
-//     document.getElementById("Latest").style.display = "none";
-// }
-
-
-// document.getElementById("selleritems").onclick = function() {
-//     document.getElementById("featureditems").style.display = "none";
-//     document.getElementById("Latest").style.display = "none";
-// }
-
-
-// document.getElementById("Latest").onclick = function() {
-//     document.getElementById("featureditems").style.display = "none";
-//     document.getElementById("selleritems").style.display = "none";
-// }
-
-
